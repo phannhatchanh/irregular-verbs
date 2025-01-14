2d7be8d3a2eb3f48bd0275eecc888f4e38032237
Phan Nhat Chanh <phannhatchanh@gmail.com>
2025-01-14 21:29:58 +0700

    chore: component styling

--- components/search-input.tsx
+++ components/search-input.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Input } from "@/components/ui/input";
-import { X } from "lucide-react";
+import { SearchIcon, X } from "lucide-react";
 
 interface SearchInputProps {
   searchTerm: string;
@@ -9,16 +9,21 @@ interface SearchInputProps {
 }
 
 export const SearchInput: React.FC<SearchInputProps> = ({ searchTerm, setSearchTerm, clearSearch }) => (
-  <div className="relative w-full print:hidden">
+  <div className="relative w-full flex flex-1 flex-shrink-0 print:hidden">
+    <label htmlFor="search" className="sr-only">
+      Search
+    </label>
+    <SearchIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2" />
     <Input
       type="text"
       placeholder="Nhập động từ cần tìm..."
       value={searchTerm}
       onChange={(e) => setSearchTerm(e.target.value)}
+      className="px-10 text-pink-600"
     />
     {searchTerm && (
       <button onClick={clearSearch} className="absolute right-2 top-[18px] -translate-y-1/2 hover:opacity-70">
-        <X />
+        <X size="20" />
       </button>
     )}
   </div>
--- components/vocabulary.tsx
+++ components/vocabulary.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { SearchInput } from "@/components/search-input";
 import { VerbCell } from "@/components/verb-cell";
 import { useVocabularySearch } from "@/hooks/use-vocabulary-search";
@@ -12,40 +11,49 @@ export function Vocabulary() {
   return (
     <>
       <SearchInput searchTerm={searchTerm} setSearchTerm={setSearchTerm} clearSearch={clearSearch} />
-      <Table className="visible overflow-hidden rounded-md border">
-        <TableCaption>Common irregular verbs.</TableCaption>
-        <TableHeader>
-          <TableRow>
-            <TableHead className="bg-blue-600 font-bold text-white">INFINITIVE</TableHead>
-            <TableHead className="bg-green-600 font-bold text-white">PAST SIMPLE</TableHead>
-            <TableHead className="bg-pink-600 font-bold text-white">PAST PARTICIPLE</TableHead>
-            <TableHead className="bg-gray-600 font-bold text-white">MEANING</TableHead>
-          </TableRow>
-        </TableHeader>
+      <div className="relative grid grid-cols-1 gap-2">
+        <div className="print:top-auto sticky top-2 z-20 grid grid-cols-4 text-center gap-2 text-sm md:text-base font-medium">
+          <div className="bg-blue-600 text-white p-2 rounded">
+            <span className="sm:block hidden">INFINITIVE</span>
+            <span className="sm:hidden block">V1</span>
+          </div>
+          <div className="bg-green-600 text-white p-2 rounded">
+            <span className="sm:block hidden">PAST SIMPLE</span>
+            <span className="sm:hidden block">V2</span>
+          </div>
+          <div className="bg-pink-600 text-white p-2 rounded">
+            <span className="sm:block hidden">PAST PARTICIPLE</span>
+            <span className="sm:hidden block">V3</span>
+          </div>
+          <div className="bg-gray-600 text-white p-2 rounded">MEANING</div>
+        </div>
+
         {Object.entries(groupedData).map(([letter, items]) => (
-          <TableBody key={letter}>
-            <TableRow id={`group-${letter}`} className="border-t bg-slate-100">
-              <TableCell colSpan={4} className="text-center text-2xl font-bold">
-                {letter}
-              </TableCell>
-            </TableRow>
+          <div key={letter} className="space-y-2">
+            <div className="print:top-auto sticky top-[56px] z-10 bg-gray-100 p-2 text-center font-semibold text-lg rounded">
+              {letter}
+            </div>
             {items.map((item: VerbItem, index: number) => (
-              <TableRow key={index}>
-                <TableCell>
+              <div
+                key={index}
+                className="grid grid-cols-4 gap-2 p-2 hover:bg-gray-100 rounded border text-sm md:text-base"
+              >
+                <div className="text-blue-600">
                   <VerbCell colorVariant="blue" verbForm={item.infinitive} />
-                </TableCell>
-                <TableCell>
+                </div>
+                <div className="text-green-600">
                   <VerbCell colorVariant="green" verbForm={item.past_simple} />
-                </TableCell>
-                <TableCell>
+                </div>
+                <div className="text-pink-600">
                   <VerbCell colorVariant="pink" verbForm={item.past_participle} />
-                </TableCell>
-                <TableCell className="font-semibold">{item.meaning}</TableCell>
-              </TableRow>
+                </div>
+                <div className="flex items-center lowercase">{item.meaning}</div>
+              </div>
             ))}
-          </TableBody>
+          </div>
         ))}
-      </Table>
+      </div>
+      <div className="m-auto text-slate-500">Common irregular verbs.</div>
     </>
   );
 }
